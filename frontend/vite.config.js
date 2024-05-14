edda528cd3d53b45744082eada16c5bc488b427c
Mahesh Chopade <65208771+mahesh2-lab@users.noreply.github.com>
2024-05-14 14:32:39 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target:"https://chatapp-c4bd.onrender.com/login",
+        target:"https://chatapp-c4bd.onrender.com",
       },
     },
   },
